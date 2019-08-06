88dbe8f1d21d0f406aca4c79824570d321da29ac
Iváne <iromcan1984@gmail.com>
2019-08-06 15:43:22 +0200

    Listo para merge

--- VideoBeta/routes/roles.routes.js
+++ VideoBeta/routes/roles.routes.js
@@ -24,7 +24,7 @@ router.get('/miPerfil', checkRoles("GUEST"), (req, res, next) => {
     axiosMovies.get(`/upcoming?api_key=${apiKey}&language=${apiLanguage}&page=${page}&region=${apiRegion}`) // método de la API
         .then(response => {
             //   console.log(response.data.results)
-            res.render('roles/guest', { response: response.data.results })
+            res.render('roles/profile', { response: response.data.results })
         })
         .catch(error => {
             console.log(error)
