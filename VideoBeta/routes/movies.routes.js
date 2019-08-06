f8cace82e605e4e726886f9d650fd661cc25e9df
Iváne <iromcan1984@gmail.com>
2019-08-06 16:32:17 +0200

    layout eliminado

--- VideoBeta/routes/movies.routes.js
+++ VideoBeta/routes/movies.routes.js
@@ -18,57 +18,35 @@ const axiosMovies = axios.create({
 
 
 // Detalles de la pelicula
-<<<<<<< HEAD
-router.get('/detail', (req, res, next) => {
-
-  //const Id = document.getElementById("id").value
-  //const Over = document.getElementById("over")
-  //const Title = document.getElementById("title")
-
-  axiosMovies.get(`/${Id}?api_key=${apiKey}&language=${apiLanguage}&page=${page}&region=${apiRegion}`)
-    .then(response => {
-
-      res.render('movies/movies-detail', { response: response.data.results })
-    })
-    .catch(error => {
-      console.log(error)
-    })
-})
-
-
-
-
-=======
 
 router.get('/detail/:id', (req, res, next) => {
   // console.log('estoy dentro')
   // console.log(req.params.id)
   let movieId = req.params.id // este es el id que nos hemos traido como params desde el enlace de detalles del profile.hbs
   axiosMovies.get(`/${movieId}?api_key=${apiKey}&language=${apiLanguage}&page=${page}&region=${apiRegion}`)
-  .then(response => { 
-    // console.log("estoy en la promesa")
-        res.render('movies/movies-detail', {response: response.data})
-      })
-      .catch(error => {
-        console.log(error)
-      })
+    .then(response => {
+      // console.log("estoy en la promesa")
+      res.render('movies/movies-detail', { response: response.data })
+    })
+    .catch(error => {
+      console.log(error)
+    })
 
 })
 
->>>>>>> 657158edb2ea6ad27a7ddefa97d7db205be911b9
 
-router.post('/create', (req, res, next)=>{
+router.post('/create', (req, res, next) => {
 
-  const{title, overview,id,vote_average}=req.body
+  const { title, overview, id, vote_average } = req.body
 
-  Movie.create({title, overview,id,vote_average})
-  .then(theMovie => res.redirect('/roles/miPerfil') )
+  Movie.create({ title, overview, id, vote_average })
+    .then(theMovie => res.redirect('/roles/miPerfil'))
 })
 
 router.get('/mis_favoritos', (req, res, next) => {
   Movie.find()
-  .then((myMovies => res.render('movies/movies-list', {movies: myMovies})))
-  .catch((err) => console.log(err))
+    .then((myMovies => res.render('movies/movies-list', { movies: myMovies })))
+    .catch((err) => console.log(err))
 })
 
 
